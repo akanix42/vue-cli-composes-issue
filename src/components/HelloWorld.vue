edef3dffdd58347878173886ef815ddd22d3008e
Nathan Reid <nathan-code@nathantreid.com>
2018-05-11 08:34:02 -0500

    load css module with composes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1 :class="$style.foo">{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -41,7 +41,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style module lang="scss">
+.foo {
+  composes: test from "./test.module.scss";
+}
+
 h3 {
   margin: 40px 0 0;
 }
